b862c1ca2ae2bd70a7af2f490da86b7081958123
elham aghabarari <elham.aghabarari@gmail.com>
2024-01-16 17:17:41 +1100

    change log

--- cpp20_test/cpp20_test.cpp
+++ cpp20_test/cpp20_test.cpp
@@ -47,7 +47,7 @@ int main()
     std::jthread(fillArray, 20000);
     std::jthread(readArray,0);
     
-
+    std::cout << "eeeeeeeeeeeeeend";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
